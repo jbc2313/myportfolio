72f94b2f563760ed7c05c9f0ae9bc33d5c042eb4
jbc2313 <jamesbcorbin@gmail.com>
2022-08-11 12:13:21 -0500

    added tripcommander and links to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   
@@ -19,19 +19,46 @@ const Projects = () => {
     grid-template-columns: [first] auto;
     grid-template-rows: [row1-start] 20% [row2-start] 50% [row3-start] 30%;
     width: 200px;
+      a {
+        text-decoration: none;
+        color: black;
+      }
   `
   
   return (
     <ProjectsPage>
       <Card>
-        <p style={{borderBottom: 'solid black 5px', textAlign: 'center' }}>Roulette</p>
+        <p style={{borderBottom: 'solid black 5px', textAlign: 'center' }}>
+          <a href='https://jbc2313.github.io/Roulette/'>
+            Roulette
+          </a>
+        </p>
         <img width='100%' height='100%' src='https://res.cloudinary.com/dhe2c2/image/upload/v1658207226/html_css_trjtdk.png' alt='htmlcss' />
         <p style={{borderTop: 'solid black 5px'}}>This is info about the project</p>
       </Card>
       <Card>
-        <p style={{borderBottom: 'solid black 5px', textAlign: 'center' }}>Ineftee</p>
+        <p style={{borderBottom: 'solid black 5px', textAlign: 'center' }}>
+          <a href='https://ineftee.herokuapp.com/'>
+            Ineftee
+          </a>
+        </p>
         <img width='100%' height='100%' src='https://res.cloudinary.com/dhe2c2/image/upload/v1658207221/nodejs_cuy6xg.png' alt='nodejs' />
         <p style={{borderTop: 'solid black 5px'}}>This is info about the project</p>
+      </Card>
+      <Card>
+        <p
+        style={{borderBottom: 'solid black 5px', textAlign: 'center'}}
+        >
+          <a href='https://trip-commander.herokuapp.com/'>
+            Trip Commander
+          </a>
+        </p>
+        <img width='100%' height='100%' src='https://res.cloudinary.com/dhe2c2/image/upload/v1660234322/reacticon_bivaun.svg' alt='reactjs' />
+        <p
+        style={{borderTop: 'solid black 5px'}}
+        >
+          This is info about my team project!
+        </p>
       </Card>
       
 
